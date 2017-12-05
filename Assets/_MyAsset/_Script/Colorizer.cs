75920e513088ffd58728373a2211d756f4323c7c
Anthony Castor <Anthony Castor>
2017-12-05 16:15:35 +0800

    first commit

---
+++ Assets/_MyAsset/_Script/Colorizer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Colorizer : EditorWindow {
+	Color color;
+
+	[MenuItem("Window/Colorizer")]
+	public static void ShowWindow(){
+		GetWindow<Colorizer>("Colorizer");
+	}
+
+	void OnGUI(){
+		GUILayout.Label("Color Selected Objects!", EditorStyles.boldLabel);
+		color = EditorGUILayout.ColorField("Color", color);
+		if(GUILayout.Button("COLORIZE!")){
+			FuncColorize();
+		}
+	}
+
+	void FuncColorize(){
+		foreach(GameObject obj in Selection.gameObjects){
+			Renderer renderer = obj.GetComponent<Renderer>();
+			if(renderer != null){
+				renderer.sharedMaterial.color = color;
+			}
+		}
+	}
+
+
+}
---
+++ Assets/_MyAsset/_Script/Personalize.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Personalize : EditorWindow {
+
+	string myString = "Hello, World!";
+
+	[MenuItem("Window/Example")]
+	public static void ShowWindow(){
+		GetWindow<Personalize>("Example");
+	}
+
+	void OnGUI(){
+		GUILayout.Label("This is a Label", EditorStyles.boldLabel);
+
+		myString = EditorGUILayout.TextField("Name", myString);
+
+		if(GUILayout.Button("Press Me")){
+			Debug.Log("Button was Pressed");
+		}
+	}
+
+
+}
